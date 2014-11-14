f53208c5d68f746cd32fec720a015a05bd89694b
vigov5 <nguyenanhtien2210@gmail.com>
2014-11-14 23:16:17 +0900

    add simple logging

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ from app import app
 from app import app as application
 
 if __name__ == '__main__':
+    import logging
+    logging.basicConfig(filename='error.log',level=logging.DEBUG)
     app.run(host='0.0.0.0', debug = True)
